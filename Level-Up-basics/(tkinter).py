cc92b51dba7e3c040cf06a007eadfced94f4d677
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-08 15:03:06 -0800

    another commit

---
+++ Level-Up-basics/(tkinter).py
@@ -0,0 +1,21 @@
+
+# tkinter is a python library that enables a window with widgets or also called GUI(graphical user interface)
+
+
+
+from tkinter import *
+
+window_object= Tk()
+
+btn= Button(window_object,background='red',text='A cool button ',font='Tahoma',borderwidth= 2)
+
+# btn.pack()
+btn.grid(row=0,column=0)
+
+entry= Entry(window_object)
+entry.grid(row=0,column=1)
+
+text= Text(window_object,height=1,width=4)
+text.grid(row=0,column=2)
+
+window_object.mainloop()
