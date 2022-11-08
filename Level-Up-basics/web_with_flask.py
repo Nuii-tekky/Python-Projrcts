e0e03b040454f5330b1c57014fc3b16fe7eb1a15
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-08 13:18:59 -0800

    another commit

--- Level-Up-basics/web_with_flask.py
+++ Level-Up-basics/web_with_flask.py
@@ -1,11 +1,11 @@
 
-from flask import Flask
+from flask import Flask,render_template
 
 website= Flask(__name__)
 @website.route('/')
 
-def index():
-  return 'My Succesful web content'
+def index_page():
+  return render_template('index.html')
 
 if __name__== '__main__':
   website.run(debug=True)
