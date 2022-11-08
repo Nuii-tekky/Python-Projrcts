921d90d59a43fcb8cb513b3e10cd345c5409b904
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-08 15:25:51 -0800

    yet another commit

--- Level-Up-basics/(tkinter).py
+++ Level-Up-basics/(tkinter).py
@@ -1,22 +1,23 @@
 # tkinter is a python library that enables a window with widgets or also called GUI(graphical user interface)
 
+
 from tkinter import *
 window_object= Tk()
-
-def display_text():
-  print('Yayayayay')
-
 
 
 btn= Button(window_object,background='red',text='A cool button ',font='Tahoma',borderwidth= 2,command=display_text)
 
 btn.grid(row=0,column=0)
 
-entry= Entry(window_object)
+entry_value= StringVar()
+entry= Entry(window_object,textvariable=entry_value)
 entry.grid(row=0,column=1)
 
 text= Text(window_object,height=1,width=4)
 text.grid(row=0,column=2)
+
+def display_text():
+  print(entry_value.get())
 
 
 window_object.mainloop()
