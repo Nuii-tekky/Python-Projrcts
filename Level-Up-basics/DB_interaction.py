c4cf0c10d8b6944fd6e0eb4ba9efe60279038b2b
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-09 12:30:53 -0800

    commit

--- Level-Up-basics/DB_interaction.py
+++ Level-Up-basics/DB_interaction.py
@@ -12,11 +12,38 @@
 # Lastly we have to close the connection
 
 import sqlite3
+def create_table():
+  connection_object= sqlite3.connect('sample.db')
+  cursor_object= connection_object.cursor()
+  cursor_object.execute("CREATE TABLE IF NOT EXISTS goods (description TEXT, Quantity INT, Price REAL)")
+  connection_object.commit()
+  connection_object.close()
 
-connection_object= sqlite3.connect('sample.db')
-cursor_object= connection_object.cursor()
-cursor_object.execute("CREATE TABLE IF NOT EXISTS goods (Description TEXT, Quantity INT, Price REAL, Total INT)")
-cursor_object.execute("INSERT INTO goods VALUES('Cement',400,2000.05)")
+def insert_values(description,quantity,price):
+  connection_object= sqlite3.connect('sample.db')
+  cursor_object= connection_object.cursor()
+  cursor_object.execute("INSERT INTO goods VALUES(?,?,?)",(description,quantity,price))
+  connection_object.commit()
+  connection_object.close()
 
-connection_object.commit()
-connection_object.close()
+insert_values('Cement',10,2600)
+insert_values('paint_brush',5,200)
+insert_values('Mopstick',7,450)
+
+def view_db():
+  connection_object= sqlite3.connect('sample.db')
+  cursor_object= connection_object.cursor()
+  cursor_object.execute("SELECT * FROM goods")
+  rows_object= cursor_object.fetchall()
+  connection_object.close()
+  return rows_object
+
+if type(view_db())== 'list':
+  for entry in view_db():
+    print(entry)
+else:
+  print(view_db())    
+
+  
+
+
