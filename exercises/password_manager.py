79e0acfa583e2de42dd2c08a478e8fc87ac14f52
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-11 14:50:37 -0800

    hgjk

--- exercises/password_manager.py
+++ exercises/password_manager.py
@@ -1,27 +1,42 @@
-
+import os
 
 print('Welcome to the ultimate Password manager: ')
-query= input('Before we continue, type in the master password: ')
 
-def check_query(value):
-  if value == True:
-    return what_action()
-  else:
-    return 'Invalid input! ' 
-
-def what_action():
-  query2= input('Do you wanna view or add passwords (v or a) or q to quit ').lower()
+def action():
+  query= input('Before we continue, type in the master password: ')
+  query2= input('Do you wanna view or add passwords (v or a) or q to quit: ').lower()
   if query2== 'v':
     return view()
   elif query2== 'a':
     return add()
+  elif query2== 'q':
+    quit()  
   else:
     return 'invalid input! ' 
 
 
 def add():
   user_name= input('enter the account username: ')
-  passwd= input('enter the password')
+  passwd= input('enter the password: ')
 
-  with open('passwords.txt','a') as file:
-    file.write(f'{user_name} | {passwd} \n')      
+  with open(r'C:\Users\ekomobong\Documents\My Projects\Python\Python-sandboxes\passwords.txt','a') as file:
+    file.write(user_name+ "|" + passwd + '\n')
+
+def view():
+  path= r'C:\Users\ekomobong\Documents\My Projects\Python\Python-sandboxes\passwords.txt'
+
+  with open(path,'r') as file:
+    content= file.read()
+  if os.path.getsize(path) == 0:
+    query3= input('No passwords added.type a to add and q to quit: ')
+    if query3== 'a':
+      return add()
+    elif query3== 'q':
+      quit()
+    else:
+      return 'invalid input! ' 
+  else:
+    return content
+
+
+print(action())          
