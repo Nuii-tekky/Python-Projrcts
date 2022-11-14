53ae87ff2f459e576dfca886936808ceac4cf07c
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-14 13:23:48 -0800

    kdjfhk

--- rock-bottom-basics/new.py
+++ rock-bottom-basics/new.py
@@ -1,12 +1,71 @@
-user_input= input('enter a wrestler: ')
+import os
+import sys
 
-message='%s is not a good wrestler' % user_input
 
-print(message)
+with open(r'C:\Users\ekomobong\Documents\My Projects\Python\Python-sandboxes\rock-bottom-basics\files.txt','w') as new_file:
+  new_file.write(list)
 
-#--------------------------------------------------------------------------
 
-name= input('Please insert your first-name')
-surname= input('Please insert your last-name')
-hobby= input('Whats your hobby')
-status= input('is a dog an animal')
+list= []
+
+current_dir= os.getcwd()
+for files in os.listdir(current_dir):
+  list.append(files)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# user_input= input('enter a wrestler: ')
+
+# message='%s is not a good wrestler' % user_input
+
+# print(message)
+
+# #--------------------------------------------------------------------------
+
+# name= input('Please insert your first-name')
+# surname= input('Please insert your last-name')
+# hobby= input('Whats your hobby')
+# status= input('is a dog an animal')
