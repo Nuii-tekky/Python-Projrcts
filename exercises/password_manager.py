5a5572343dcde863561eb1ef6098d8d8cecc9d39
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-14 16:00:01 -0800

    kuysdghj

--- exercises/password_manager.py
+++ exercises/password_manager.py
@@ -1,6 +1,6 @@
 import os
 import random
-
+from datetime import datetime as dt
 
 print('Welcome to The ultimate Password Manager: ')
 print('What do you wanna do? ')
@@ -10,7 +10,7 @@ def input_checker(value):
   if value== 'g':
     return generate_passwd()
   elif value=='v':
-    pass
+    return view_passwd()
   elif value=='a':
     pass
   else:
@@ -18,11 +18,9 @@ def input_checker(value):
   
 
 def generate_passwd():
-  numbers= '123456789'
-  characters= '~!@#$%^&*()__+/*-?:"'
-
   def generator(value):
     return 'hello'
+    
 
   print('Enter a random word.We will build your secure password from there... ')
   query_2= input('Take Note: This is not your password.It only helps us to generate a strong one: ').lower()
@@ -43,6 +41,29 @@ def generate_passwd():
       print( '...........your password is being generated ' + "\n" )
       return generator(query_2)
       break
-   
+       
+def view_passwd():
+  path= r'C:\Users\ekomobong\Documents\My Projects\Python\Python-sandboxes\exercises\passwords.txt'
+  if os.path.exists(path):
+    with open(path,'r+') as passwd_line:
+      pass
+    print('Your saved passwords will now be shown: ' + "\n")
+  else:
+    print(' checking......................')
+    print(' Oops!! No saved passwords.')
+    query_4= input("You can add passwords here.just type 'a' to add or 'q' to quit: ").lower()
+    if query_4=='a':
+      return add_passwd()
+    elif query_4== 'q':
+      quit()  
+    else:
+      print('You failed to follow instructions! ')
+      return 'Your session has been wiped out! '      
+  
+
+def add_passwd():
+  return 'hello'
+
 
 print(input_checker(query))
+ 
