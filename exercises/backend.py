62b8268c3d9f25ee90b268160fba24452f841f26
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-16 11:12:30 -0800

    fie

--- exercises/backend.py
+++ exercises/backend.py
@@ -9,7 +9,7 @@ class DataBase:
         conn.commit()
         conn.close()
 
-    def insert(title,author,year,isbn):
+    def insert(self,title,author,year,isbn):
         conn=sqlite3.connect("books.db")
         cur=conn.cursor()
         cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
@@ -25,7 +25,7 @@ class DataBase:
         conn.close()
         return rows
 
-    def search(title="",author="",year="",isbn=""):
+    def search(self,title="",author="",year="",isbn=""):
         conn=sqlite3.connect("books.db")
         cur=conn.cursor()
         cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
@@ -33,19 +33,17 @@ class DataBase:
         conn.close()
         return rows
 
-    def delete(id):
+    def delete(self,id):
         conn=sqlite3.connect("books.db")
         cur=conn.cursor()
         cur.execute("DELETE FROM book WHERE id=?",(id,))
         conn.commit()
         conn.close()
 
-    def update(id,title,author,year,isbn):
+    def update(self,id,title,author,year,isbn):
         conn=sqlite3.connect("books.db")
         cur=conn.cursor()
         cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
         conn.commit()
         conn.close()
-
-connect()
 
