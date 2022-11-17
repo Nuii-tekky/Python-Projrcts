b199a2ce0803de9799c9ed8589e457fc3017d2f7
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-17 13:51:09 -0800

    hetinjfmdfdkj

--- Level-Up-basics/videoProcess.py
+++ Level-Up-basics/videoProcess.py
@@ -1,8 +1,29 @@
 
 import cv2,time
 
+
+# to capture  1 frame at a time i.e image
+# video_obj= cv2.VideoCapture(0)
+# check,frame= video_obj.read()
+
+# time.sleep(3)
+
+# cv2.imshow('New window',frame)
+# cv2.waitKey(0)
+# video_obj.release()
+# cv2.destroyAllWindows()
+
+# capture a full video
 video_obj= cv2.VideoCapture(0)
 
-time.sleep(6)
+while True:
+  frame= video_obj.read()
+  gray_version= cv2.cvtColor(video_obj,cv2.COLOR_BGR2GRAY)
+  cv2.imshow('New window',gray_version)
+  key= cv2.waitKey(1)
+  if key== ord('s'):
+    break
 
+  
 video_obj.release()
+cv2.destroyAllWindows()
