be3b4354406abf066c8a47c353d16ad659e26232
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-18 01:17:47 -0800

    nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn

--- Python_Apps/Dictionary _ Json/dictionary.py
+++ Python_Apps/Dictionary _ Json/dictionary.py
@@ -4,33 +4,29 @@ from difflib import get_close_matches as gcm
 
 # calling the dictionary from the json file and opening it
 dictionary= json.load(open('Python_Apps\Dictionary _ Json\data.json'))
-user_word= input('enter a word: ')
+user_word= input('enter a word: ').lower()
 
 # now i get the user input and check for spelling error
-def get(user_input):
-
-  user_input=user_input.lower()
-  
-
+def Input_processor(user_input):
   if user_input in dictionary:
     return dictionary[user_input]
 
   elif len(gcm(user_input,dictionary.keys()))>0:
-    yes_no= input(f"did you mean {gcm(user_input,dictionary.keys())[0]}? If yes type 'y' or no type 'n': ")
+    yes_no= input(f"did you mean {gcm(user_input,dictionary.keys())[0]}? Type 'y' for yes and 'n' for no ")
     if yes_no== 'y':
       return dictionary[gcm(user_input,dictionary.keys()) [0]]
     elif yes_no=='n':
-      return 'word not found,please review your spellings'  
+      return 'Word not found '  
     else:
-      return 'you are a confused person'
+      return "Your'e a confused person"
       
   else: 
-    return 'the word you entered does not exist or is incorrectly spelt'
+    return 'The word you entered does not exist or is incorrectly spelt'
   
 
 # now lets store the output in a variable so we can perform actions with it
 
-output= get(user_word)
+output= Input_processor(user_word)
 # we want to display output in seperate lines in a list     
 if type(output) == list:
   for item in output: print(item)
