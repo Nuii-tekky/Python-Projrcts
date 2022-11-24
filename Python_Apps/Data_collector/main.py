b2b7a82285c38a9e3a79461844013f0687085a50
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-24 14:27:24 -0800

    kmjx,

---
+++ Python_Apps/Data_collector/main.py
@@ -0,0 +1,17 @@
+from flask import render_template,request,Flask
+
+webpage= Flask(__name__)
+
+# render home page
+@webpage.route("/")
+def render_landing_page():
+  return render_template('sample.html')
+
+# render success page
+@webpage.route("/success",methods= ['POST'])
+def render_success_page():
+  return render_template('success.html')
+
+if __name__== '__main__':
+  webpage.debug= True
+  webpage.run()
