0d78ded5050ae2fea70b3f15a4d045f0c49b6fa5
nuii-tekky <ekomobongmarcus20@gmail.com>
2022-11-24 14:44:56 -0800

    nvmcncm,mc,c

--- Python_Apps/Data_collector/main.py
+++ Python_Apps/Data_collector/main.py
@@ -5,15 +5,10 @@ webpage= Flask(__name__)
 # render home page
 @webpage.route("/")
 def render_landing_page():
+  email= request.form['email']
+  print(f'your email is {email} ')
   return render_template('sample.html')
 
-# render success page
-@webpage.route("/success",methods= ['POST'])
-def render_success_page():
-  if request.method== 'POST':
-    email= request.form['email']
-    print(f'your email is {email} ')
-    return render_template('success.html')
   
 if __name__== '__main__':
   webpage.debug= True
