952a7baaf70d35dd422cb2f62debdef42e1f33f4
ekomobong <ekomobongmarcus20@gmail.com>
2022-11-25 12:25:46 -0800

    committed

--- Python_Apps/Data_collector/main.py
+++ Python_Apps/Data_collector/main.py
@@ -1,21 +1,23 @@
 from flask import render_template,request,Flask
 
+
 webpage= Flask(__name__)
+
 
 # render home page
 @webpage.route("/")
 def render_landing_page():
-  
-  return render_template('sample.html')
+    return render_template('sample.html')  
 
-
-@webpage.route("/success.html",methods=['post'])
+@webpage.route("/success.html",methods= ['post'])
 def render_success_page():
-  if request.method== 'post':
-    email= request.form["email"]
-    print(f'your email is {email} ')
+    email= request.form['email']
+    print(email)
     return render_template('success.html')
+
 
 if __name__== '__main__':
   webpage.debug= True
   webpage.run()
+
+
